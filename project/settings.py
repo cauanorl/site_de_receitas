7b5fc3770ed034c75f4e91950f2bb84181a2548c
Cauanorl <rodriguescauan701@gmail.com>
2022-09-22 19:55:55 -0300

    configurando arquivos estaticos

--- project/settings.py
+++ project/settings.py
@@ -129,3 +129,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Media
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = "media/"
