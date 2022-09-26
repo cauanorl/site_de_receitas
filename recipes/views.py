208786b0e494defbf976472770daed7d9882bfc0
Cauanorl <rodriguescauan701@gmail.com>
2022-09-26 18:27:58 -0300

    adicionando a página de detalhes

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,20 @@
 from django.views.generic.base import TemplateView, View
-from django.http import HttpResponse
+from __localcode.main import make_recipe
 
-# Create your views here.
+
 class Home(TemplateView, View):
     template_name = 'recipes/pages/home.html'
 
     def get(self, request, *args, **kwargs):
-        return self.render_to_response({'teste': 'home'})
+
+        return self.render_to_response({
+            'recipes': [make_recipe() for _ in range(16)],
+            'is_list_page': True,
+        })
 
 
 class DetailRecipe(TemplateView, View):
     template_name = 'recipes/pages/detail.html'
 
     def get(self, request, pk, *args, **kwargs):
-        return self.render_to_response({})
+        return self.render_to_response({'recipe': make_recipe()})
