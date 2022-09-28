f32f1a03c0bda443a25a65f5fd5ed6ee289a4266
Cauanorl <rodriguescauan701@gmail.com>
2022-09-27 21:34:25 -0300

    adicionando os models category e recipe

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.PositiveIntegerField()
+    preparation_time_unit = models.CharField(
+        choices=[
+            ('M', _('Minutes')),
+            ('H', _('Hours')),
+        ], max_length=1)
+
+    servings = models.PositiveIntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to="recipes/covers/%Y/%m/%d/")
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True)
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="recipes")
